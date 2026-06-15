729f9939c36ce40ee9ddf057a2b6a10b777b3cf0
ddunddunlee <122351788+ddunddunlee@users.noreply.github.com>
2026-06-15 18:15:07 +0900

    Add files via upload

--- shared/sidebar.js
+++ shared/sidebar.js
@@ -33,6 +33,10 @@
               badge: 'urgentBadge'   /* 긴급 배지 ID */
             },
             {
+              nav: 'personal.html', label: '개인 트래커',
+              icon: '<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/><path d="M16 11l2 2 3-3"/>'
+            },
+            {
               nav: 'report.html', label: '주간보고',
               icon: '<path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>'
             },
